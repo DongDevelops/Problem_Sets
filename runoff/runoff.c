680379118bba74bd5b4c0ea4dcb43ee393b9ccd1
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-11-28 04:04:44 +0000

    Mon, Nov 28, 2022, 4:04 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -181,7 +181,7 @@ int find_min(void)
     {
         if (minivotes > candidates[i].votes)
         {
-            minivotes = candidates[i].votes
+            minivotes = candidates[i];
         }
     }
     return 0;
