76bfc9df0e7e757a5e63acc3e66ef034fa9a30f7
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-11-28 12:40:32 +0000

    Mon, Nov 28, 2022, 12:40 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    unit8_t header[n];
+    int unit8_t header[n];
     while (fread(&header, 44, 1, input))
     {
         fwrite(&header, 44, 1, output);
