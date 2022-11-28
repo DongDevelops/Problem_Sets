370e9cb7de42768d00311f6bd560dd875992bb54
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-11-28 03:37:43 +0000

    Mon, Nov 28, 2022, 3:37 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -154,7 +154,13 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    for (int maxvotes = 0, int i = 0; i < candidate_count; i++)
+    {
+        if (maxvotes < candidates[i].votes)
+        {
+            maxvotes = candidates[i].votes;
+        }
+    }
     return false;
 }
 
