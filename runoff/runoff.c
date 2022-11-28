6ae4a331759acd7498a764ab410f8ddabcc18f0a
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-11-28 04:35:23 +0000

    Mon, Nov 28, 2022, 4:35 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -188,12 +188,11 @@ int find_min(void)
 }
 
 // Return true if the election is tied between all candidates, false otherwise
-bool is_tie(int min)
+bool is_tie(int min, int maxvotes)
 {
     for (int i = 0; i < candidate_count; i++)
-        if (min = candidates[i].votes)
+        if (min = maxvotes)
         {
-
             return true;
         }
     return false;
