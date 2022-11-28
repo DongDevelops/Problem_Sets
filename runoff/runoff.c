4b8424cfd6e71460d93b0bb1b52dd3ce55b2b1de
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-11-28 05:09:15 +0000

    Mon, Nov 28, 2022, 5:09 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -129,8 +129,11 @@ bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        preferences[voter][rank] = i;
-        return true;
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            preferences[voter][rank] = i;
+            return true;
+        }
     }
     return false;
 }
