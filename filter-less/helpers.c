a229f13af93d151fc5c53f0454921efc9d21824e
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-11-30 10:48:32 +0000

    Wed, Nov 30, 2022, 10:48 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -4,6 +4,12 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtBlue
+        }
+    }
     return;
 }
 
