8bbd79a3ceddeff61d1e3fb7cb16939edd085649
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-11-30 11:08:25 +0000

    Wed, Nov 30, 2022, 11:08 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -10,7 +10,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j] = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3.0);
+            image[i][j].rgbtBlue = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3.0);
+            image[i][j].rgbtGreen = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3.0);
+            image[i][j].rgbtRed = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3.0);
         }
     }
     return;
