cbebc5c5bb0900f905771703457130c7b565cb07
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-01 01:35:37 +0000

    Thu, Dec 1, 2022, 1:35 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -28,9 +28,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtRed = imin(round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue), 255);
-            image[i][j].rgbtGreen = imin(round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue), 255);
-            image[i][j].rgbtBlue = imin(round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue), 255);
+            int sepiaRed = imin(round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue), 255);
+            int sepiaGreen = imin(round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue), 255);
+            int sepiaBlue = imin(round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue), 255);
+
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
         }
     }
     return;
