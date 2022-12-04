eac1d64f9a6792a250c56ecb66302e0d9b6ecf6f
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-04 13:40:17 +0000

    Sun, Dec 4, 2022, 1:40 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -57,7 +57,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+     
     return;
 }
 
