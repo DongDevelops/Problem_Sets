879696ea441d2a655259b74a712d6fbc761298fe
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-05 08:38:09 +0000

    Mon, Dec 5, 2022, 8:38 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
         int count = 0;
         while(fread(buffer, 512, 1, argv[1]))
         {
-            if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+            if(buffer[n] == 0xff && buffer[n+1] == 0xd8 && buffer[n+2] == 0xff && (buffer[n+3] & 0xf0) == 0xe0)
             {
                 sprintf(filename, "%03i.jpg", count);
                 count ++;
