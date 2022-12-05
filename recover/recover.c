b2cdd1a20fae99c695555d7ed613c6bd477d97ae
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-05 09:25:24 +0000

    Mon, Dec 5, 2022, 9:25 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -9,38 +9,38 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    FILE *file = fopen(argv[1], "r");
-    if (file != NULL)
+    FILE *buffer = fopen(argv[1], "r");
+    if (buffer == NULL)
     {
-        int *buffer[];
-        if(buffer == NULL)
-        {
-            return 1;
-        }
+        printf("Correct usage: ./recover IMAGE\n");
+        return 1;
+    }
 
-        int count = 0;
+    int count = 0;
 
-        while(fread(buffer, 512, 1, argv[1]))
+    while(fread(buffer, 512, 1, argv[1]))
+    {
+        for(int n = 0; n < 512, n++);
         {
-            for(int n = 0; n < 512, n++);
+            if(buffer[n] == 0xff && buffer[n+1] == 0xd8 && buffer[n+2] == 0xff && (buffer[n+3] & 0xf0) == 0xe0)
             {
-                if(buffer[n] == 0xff && buffer[n+1] == 0xd8 && buffer[n+2] == 0xff && (buffer[n+3] & 0xf0) == 0xe0)
-                {
-                    sprintf(filename, "%03i.jpg", count);
-                    count ++;
+                sprintf(filename, "%03i.jpg", count);
+                count ++;
 
-                    FILE *img = fopen("%03i.jpg", "w");
-                    if(img == NULL)
-                    {
-                        return 3;
-                    }
-                    fwrite(%03i.jpg, 512, 1, buffer);
-                }
-                else if(buffer[n] == 0)
+                FILE *img = fopen("%03i.jpg", "w");
+                if(img == NULL)
                 {
-                    return 1;
+                    return 3;
                 }
+
+                fwrite(%03i.jpg, 512, 1, buffer);
             }
+
+            else if(buffer[n] == 0)
+            {
+                return 1;
+            }
+            
         }
     }
 }
