510670eec5aa27fe2c061101276b150357860c69
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-05 06:30:56 +0000

    Mon, Dec 5, 2022, 6:30 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -10,12 +10,15 @@ int main(int argc, char *argv[])
     }
 
     FILE *file = fopen(argv[1], "r");
-    int buffer[];
-    while(fread(buffer, 512, 1, argv[1]))
+    if (file != NULL)
     {
-        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        int buffer[];
+        while(fread(buffer, 512, 1, argv[1]))
         {
+            if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+            {
 
+            }
         }
     }
 }
