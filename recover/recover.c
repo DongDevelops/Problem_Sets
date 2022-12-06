a9ea61ebc68b017265308b3f487aed3afa17c2a4
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-06 02:41:18 +0000

    Tue, Dec 6, 2022, 2:41 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -33,7 +33,10 @@ int main(int argc, char *argv[])
 
     while(fread(buffer, sizeof(char), 512, inputPtr) == 512);
     {
-        if(buffer[0])
+        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            sprintf()
+        }
     }
 
 
