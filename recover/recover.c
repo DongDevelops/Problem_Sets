af3e76b01063a9f87e143f706dc92ed4e284903b
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-06 01:34:45 +0000

    Tue, Dec 6, 2022, 1:34 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -10,39 +10,5 @@ int main(int argc, char *argv[])
     }
 
 
-    FILE *buffer = fopen(argv[1], "r");
-    int *rFile = malloc(sizeof(argv[1]));
-    if (buffer == NULL)
-    {
-        printf("Correct usage: ./recover IMAGE\n");
-        return 1;
-    }
 
-    int count = 0;
-
-    while(fread(rFile, 512, 1, buffer))
-    {
-        for(int n = 0; n < 512; n++)
-        {
-            if(rFile[n] == 0xff && rFile[n+1] == 0xd8 && rFile[n+2] == 0xff && (rFile[n+3] & 0xf0) == 0xe0)
-            {
-                sprintf(rFile, "%03i.jpg", count);
-                count ++;
-                continue;
-                FILE *img = fopen("%03i.jpg", "w");
-                if(img == NULL)
-                {
-                    return 1;
-                }
-
-                fwrite(, 512, 1, img);
-            }
-
-            else if(rFile[n] == 0)
-            {
-                return 1;
-            }
-        }
-        free(rFile);
-    }
 }
