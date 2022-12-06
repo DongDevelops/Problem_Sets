64fefd276d5b27700e3019c4588b6f5263a62358
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-06 01:43:07 +0000

    Tue, Dec 6, 2022, 1:43 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,9 +6,14 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         printf("Correct usage: ./recover IMAGE\n");
-        return 1 ;
+        return 1;
     }
 
-
+    char* inputFile = argv[1];
+    if(inputFile == NULL)
+    {
+        printf("Cannot open file: %s\n", inputFile);
+        return 1;
+    }
 
 }
