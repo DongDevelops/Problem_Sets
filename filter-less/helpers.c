1496e813500bacff77466d6e6c26917eb4eacfac
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-06 05:39:37 +0000

    Tue, Dec 6, 2022, 5:39 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -57,12 +57,12 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width/2; j++)
         {
-            image *[i][j];
+            image[i][j].rgbtRed = image;
             image *[i][width-(j+1)];
             image tmp = *[i][j];
             *[i][j] = *[i][width-(j+1)];
