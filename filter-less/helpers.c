e05b990d191b8694e9a1bb2f9ab32e2d6c23e490
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-06 05:55:19 +0000

    Tue, Dec 6, 2022, 5:55 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -79,6 +79,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j].rgbtRed = image[i][j].rgbtRed;
+            copy[i][j].rgbtGreen = image[i][j].rgbtGreen;
+            copy[i][j].rgbtBlue = image[i][j].rgbtBlue;
+        }
+    }
     
     return;
 }
