805bca1ea9ac76b9b4a4d5f83aee0e2f1c9c9727
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-08 03:01:41 +0000

    Thu, Dec 8, 2022, 3:01 AM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -46,7 +46,7 @@ person *create_family(int generations)
     if (generations > 1)
     {
         // Create two new parents for current person by recursively calling create_family
-        person *parent0 = create_family(generations - 1);
+        person *parents[0] = create_family(generations - 1);
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
