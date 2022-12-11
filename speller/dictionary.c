f3125ae074174b1d53e523fc0da1ca36678d194f
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-11 12:07:25 +0000

    Sun, Dec 11, 2022, 12:07 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -34,11 +34,13 @@ unsigned int hash(const char *word)
     return toupper(word[0]) - 'A';
 }
 
+int word_count = 0;
+
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
     // TODO
-    int word_count = 0;
+
     FILE* file = fopen(dictionary, "r");
     if(file == NULL)
     {
