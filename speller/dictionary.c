dfd928eeb7a48e8c5573a0783a2ced3d9d9c29ae
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-11 13:22:29 +0000

    Sun, Dec 11, 2022, 1:22 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -34,7 +34,8 @@ unsigned int hash(const char *word)
 
     int i = word[0] % 26;
     N = i;
-    node word = malloc(sizeof(node));
+    node* word = malloc(sizeof(node));
+    word -> next = 
 
     return N;
 }
