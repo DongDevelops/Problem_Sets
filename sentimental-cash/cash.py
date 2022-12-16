c83bbd0bd6016675847843dc06be9a08abea5528
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-16 11:42:24 +0000

    Fri, Dec 16, 2022, 11:42 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -5,3 +5,24 @@ while True:
     if change > 0
         break
 
+count = 0
+
+while change > 0.25:
+    change = change - 0.25
+    count += 1
+    return change
+
+while change > 0.10:
+    change = change - 0.10
+    count += 1
+    return change
+
+while change > 0.05:
+    change = change - 0.05
+    count += 1
+    return change
+
+**For example, 45 was given
+25, 10, 5, 1
+
+45 - 25 = 20
