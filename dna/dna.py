dcf42d65f17325dd7c460979011cb0ee38c88f09
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-18 10:48:42 +0000

    Sun, Dec 18, 2022, 10:48 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -10,14 +10,14 @@ def main():
 
     # TODO: Read database file into a variable
     database = []
-    with open(sys.argv[1], 'r') as file:
+    with open(sys.argv[1]) as file:
         reader = csv.DictReader(file)
         for row in reader:
             database.append(row)
 
 
     # TODO: Read DNA sequence file into a variable
-    with open(sys.argv[2], 'r') as dnaFile:
+    with open(sys.argv[2]) as dnaFile:
         dna_sequence = dnaFile.read()
 
     # TODO: Find longest match of each STR in DNA sequence
