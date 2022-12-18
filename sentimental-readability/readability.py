06c9d0b3c097a1aa6ce5d24f07c6560a7cff70a2
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-18 05:21:38 +0000

    Sun, Dec 18, 2022, 5:21 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,13 +2,20 @@ from cs50 import get_string
 
 userInput = get_string("Text: ")
 
+L = 0
+S = 0
 
-Calculate = 0.0588 * L - 0.296 * S - 15.8
 
-if >= 16:
+L += 1
+
+S += 1
+
+calculate = 0.0588 * L - 0.296 * S - 15.8
+
+if calculate >= 16:
     print("Grade 16+")
 
-if < 1:
+if calculate < 1:
     print("Before Grade 1")
 
 
