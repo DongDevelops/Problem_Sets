2b5822fd61de8090492fb10fba0ac3a0d2d00511
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-18 09:20:18 +0000

    Sun, Dec 18, 2022, 9:20 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -9,9 +9,12 @@ def main():
         print("Incorrect usage")
 
     # TODO: Read database file into a variable
-    with open(sys.argv[1], 'r') as file
-    reader = csv.DictReader(file)
-    next(reader)
+    database = []
+    with open(sys.argv[1], 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            database.append(row)
+        
 
     # TODO: Read DNA sequence file into a variable
     with open(sys.argv[2], 'r') as dnaFile:
