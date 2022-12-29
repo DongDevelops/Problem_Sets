a275d7c063ea064172f425b791076cfb8b465361
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-29 04:45:44 +0000

    Thu, Dec 29, 2022, 4:45 AM +00:00

--- homepage/js.js
+++ homepage/js.js
@@ -7,7 +7,17 @@ var myQuestions = [
         c: '115'
       },
       correctAnswer: 'b'
+    },
+    {
+      question: "What is 30/3?",
+      answers: {
+        a: '3',
+        b: '5',
+        c: '10'
+      },
+      correctAnswer: 'c'
     }
+];
 
 
     var quizContainer = document.getElementById('quiz');
