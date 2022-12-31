c613b3a34f6026e5988bccb38ffa8b9f68dfc3a0
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2022-12-31 08:44:20 +0000

    Sat, Dec 31, 2022, 8:44 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -115,7 +115,7 @@ def quote():
         symbol = request.form.get("symbol")
         price = usd(lookup(symbol)["price"])
 
-        return render_template("quoted.html", price = price)
+        return render_template("quoted.html", price=price)
 
     else:
         return render_template("quote.html")
