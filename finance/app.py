ce98c52c8d21dff1cc301b7f0fd4fc6f10dee7c7
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-01-03 14:10:51 +0000

    Tue, Jan 3, 2023, 2:10 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -167,6 +167,8 @@ def register():
         if not request.form.get("username"):
             return apology("must provide username", 400)
 
+        if request.form.get("username")
+
         if not request.form.get("password"):
             return apology("must provide password", 400)
 
