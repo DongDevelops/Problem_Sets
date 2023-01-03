3b6c2c1df38ad8b4f1312c9daf76d37a9343572d
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-01-03 05:52:15 +0000

    Tue, Jan 3, 2023, 5:52 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -167,13 +167,13 @@ def register():
     if request.method == "POST":
 
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
 
         if not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", 400)
 
         if not request.form.get("confirmation"):
-            return apology("must provide confirmation confirmation", 403)
+            return apology("must provide confirmation confirmation", 400)
 
         if request.form.get("password") != request.form.get("confirmation"):
             return apology("passwords should match", 403)
