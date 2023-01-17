e3d796d8f7cf1585b99323ea598440a57d724993
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-01-17 13:50:12 +0000

    Tue, Jan 17, 2023, 1:50 PM +00:00

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return HttpResponse("Hello!!!")
 
 def brian(request):
     return HttpResponse("Hello, Brian!")
