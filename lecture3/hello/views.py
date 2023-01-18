81e5b214cf6094b2b9aaf7b481aaf147746b928b
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-01-18 01:42:46 +0000

    Wed, Jan 18, 2023, 1:42 AM +00:00

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -8,8 +8,10 @@ def index(request):
 def brian(request):
     return HttpResponse("Hello, Brian!")
 
-def david(request): 
+def david(request):
     return HttpResponse("Hello, David!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
