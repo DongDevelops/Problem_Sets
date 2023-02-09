914c70fd2630107bd1d9e04ac55e21af1f62f6ea
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-09 01:57:26 +0000

    Thu, Feb 9, 2023, 1:57 AM +00:00

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.title, name="title")
-    path("search/<str:title>", views.search, name="search")
 ]
