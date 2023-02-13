f0750f79d6b30352ff7b01d49f07d3bb30e48436
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-13 02:28:33 +0000

    Mon, Feb 13, 2023, 2:28 AM +00:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 from .models import User, Comments, Bids, Listings
 
