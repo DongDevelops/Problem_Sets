0de8ca32079fae40fa19ff51a10a74c864818296
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-15 04:33:11 +0000

    Wed, Feb 15, 2023, 4:33 AM +00:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -100,7 +100,7 @@ def item(request, id):
         username = request.POST["username"]
         creator = request.POST["creator"]
         user = User.objects.get(username=username)
-        item = user.listings.get(id=item_id)
+        item = Listings.objects.get(id=item_id)
 
         if username == creator:
             if item.watchlist == False:
