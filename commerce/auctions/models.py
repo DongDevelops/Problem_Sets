2f661ef1d8bf54e5844c0c1191b5b20e1e989742
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-15 02:04:27 +0000

    Wed, Feb 15, 2023, 2:04 AM +00:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -8,9 +8,6 @@ class Bids(models.Model):
     def __str__(self):
         return f"{self.amount} at {self.time}"
 
-class User(AbstractUser):
-    bidders = models.ManyToManyField(Listings, blank=True, related_name="bidders")
-    pass
 
 class Listings(models.Model):
     title = models.CharField(max_length=64)
@@ -18,10 +15,14 @@ class Listings(models.Model):
     bid = models.ForeignKey(Bids, on_delete=models.CASCADE, related_name="bid")
     image = models.URLField(null=True, blank=True)
     watchlist = models.BooleanField(default=False)
-    creator = models.ManyToManyField(User, blank=True, related_name="creator")
+    listings = models.ManyToManyField(User, blank=True, related_name="listings")
 
     def __str__(self):
         return f"{self.id}: {self.title}"
+
+
+class User(AbstractUser):
+    bidders = models.ManyToManyField(Listings, blank=True, related_name="bidders")
 
 
 class Comments(models.Model):
