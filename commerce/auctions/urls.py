0d7544081be30974ce626b875d53d817cd622406
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-16 09:58:59 +0000

    Thu, Feb 16, 2023, 9:58 AM +00:00

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -8,12 +8,12 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("create", views.create, name="create"),
-    path("item/<int:item_id>", views.item, name="item"),
+    path("item/<int:listing_id>", views.item, name="item"),
     path("place_bid", views.place_bid, name="place_bid"),
     path("watchlist", views.watchlist, name="watchlist"),
     path("remove", views.remove, name="remove"),
     path("close", views.close, name="close"),
     path("closed", views.closed, name="closed"),
-    path("closed_item/<int:item_id>", views.closed_item, name="closed_item"),
-    path("<int:item_id>/comments", views.comments, name="comments")
+    path("closed_item/<int:listing_id>", views.closed_item, name="closed_item"),
+    path("<int:listing_id>/comments", views.comments, name="comments")
 ]
