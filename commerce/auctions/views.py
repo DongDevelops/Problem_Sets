8b4cb2094e7afd0eb09cca27dadcbe140436b803
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-17 12:59:38 +0000

    Fri, Feb 17, 2023, 12:59 PM +00:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -246,7 +246,7 @@ def comments(request, listing_id):
 
 def category(request):
     if request.method == "POST":
-        selected_category_id = request.POST["category.name"]
+        selected_category_id = request.POST["categories"]
         selected_category = Categories.objects.get(name=selected_category_id)
         selected_listings = Listings.objects.filter(category=selected_category).filter(active=True)
         return render(request, "auctions/category.html", {
