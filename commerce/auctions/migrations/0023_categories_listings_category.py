50a26abe2401c143e84949e3c504dbb802adbe34
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-17 02:17:52 +0000

    Fri, Feb 17, 2023, 2:17 AM +00:00

--- commerce/auctions/migrations/0023_categories_listings_category.py
+++ commerce/auctions/migrations/0023_categories_listings_category.py
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='listings',
             name='category',
-            field=models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, related_name='category', to='auctions.categories'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='category', to='auctions.categories'),
             preserve_default=False,
         ),
     ]
