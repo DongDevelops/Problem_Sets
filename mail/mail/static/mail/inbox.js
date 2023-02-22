5525607b0dcc94361d9a62b107e43ef75c0a2941
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-22 01:25:28 +0000

    Wed, Feb 22, 2023, 1:25 AM +00:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -14,24 +14,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function send_email() {
 
-  const recipients = document.querySelector('#compose-recipients').value;
-  const subject = document.querySelector('#compose-subject').value;
-  const body = document.querySelector('#compose-body').value;
+  preventDefault();
 
-    fetch('/emails', {
-      method: 'POST',
-      body: JSON.stringify({
-        recipients: 'recipients',
-        subject: 'subject',
-        body: 'body'
-      })
-    })
-    .then(response => response.json())
-    .then(result => {
-      console.log(result);
-    });
-    load_mailbox('sent');
-    return false;
+  console.log(hi);
 }
 
 
