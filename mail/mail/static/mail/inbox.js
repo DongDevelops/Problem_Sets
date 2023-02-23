f5a9e1d5e65d5246b0bf31c46bd826e03d174e5a
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-23 10:35:42 +0000

    Thu, Feb 23, 2023, 10:35 AM +00:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -92,10 +92,10 @@ document.addEventListener('DOMContentLoaded', function() {
         td1.innerHTML = email.sender;
         td2.innerHTML = email.subject;
         td3.innerHTML = email.timestamp;
-        document.querySelector('table').append(tr);
-        document.querySelector('table').append(td1);
-        document.querySelector('table').append(td2);
-        document.querySelector('table').append(td3);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        table.appendChild(tr);
       });
     })
 
