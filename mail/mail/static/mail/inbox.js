ccf2bd7a3dba71b80c8d1f6f2cc3d9e807907d99
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-23 04:48:47 +0000

    Thu, Feb 23, 2023, 4:48 AM +00:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -76,6 +76,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Show the mailbox name
     document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-    fetch(`'/emails/$mailbox')
+    fetch(`'/emails/${mailbox}'`)
+    
   }
 
