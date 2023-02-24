aec159ec32565da28a4ce6c8391a39848649a0d5
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-24 01:33:49 +0000

    Fri, Feb 24, 2023, 1:33 AM +00:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -103,9 +103,11 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     })
 
+
+    
     document.querySelectorAll('tr').forEach(tr => {
       tr.onclick = function () {
-        
+
       }
     })
 
