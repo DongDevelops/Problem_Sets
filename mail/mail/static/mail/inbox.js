8a8a637959571eedd7dc2cf34da97c7ed40129d8
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-26 13:21:25 +0000

    Sun, Feb 26, 2023, 1:21 PM +00:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
     document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
     document.querySelector('#compose').addEventListener('click', compose_email);
-    document.querySelector('tr').addEventListener('click', view_email);
+    document.querySelector('tr').addEventListener('click', view_email(email_id));
 
     // By default, load the inbox
     load_mailbox('inbox');
