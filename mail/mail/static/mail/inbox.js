3301f95a599a079b67a4b964d9ff1c0a42f1d04e
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-27 05:05:37 +0000

    Mon, Feb 27, 2023, 5:05 AM +00:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -74,6 +74,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Show the mailbox and hide other views
     document.querySelector('#emails-view').style.display = 'block';
     document.querySelector('#compose-view').style.display = 'none';
+    div.style.display = 'none';
 
     // Show the mailbox name
     document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
