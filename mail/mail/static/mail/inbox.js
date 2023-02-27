3f4a2e8a7dbc58e96037eb6609084520d2d1937e
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-27 09:34:34 +0000

    Mon, Feb 27, 2023, 9:34 AM +00:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -88,7 +88,6 @@ document.addEventListener('DOMContentLoaded', function() {
         const td1 = document.createElement('td');
         const td2 = document.createElement('td');
         const td3 = document.createElement('td');
-        
         td1.innerHTML = email.sender;
         td2.innerHTML = email.id;
         td3.innerHTML = email.read;
