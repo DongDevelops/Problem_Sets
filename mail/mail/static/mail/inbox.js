40125fef45b0cb60b9467ade16c9f3b21b9bf1cf
whatsoever22 <119066606+whatsoever22@users.noreply.github.com>
2023-02-28 06:49:58 +0000

    Tue, Feb 28, 2023, 6:49 AM +00:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -208,8 +208,8 @@ document.addEventListener('DOMContentLoaded', function() {
               document.querySelector('#compose-view').style.display = 'block';
               document.querySelector('#email-view').style.display = 'none';
 
-              document.querySelector('#compose-recipients').value = 'result.';
-              document.querySelector('#compose-subject').value = '';
+              document.querySelector('#compose-recipients').value = result.sender;
+              document.querySelector('#compose-subject').value = `Re: ${result.subject}`;
               document.querySelector('#compose-body').value = '';
             });
           }
